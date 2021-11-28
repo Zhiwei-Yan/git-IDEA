000a6a20973f068ddadd03cb5ef5a7d04e6066e0
yan <2304040167@qq.com>
2021-11-28 20:00:51 +0800

    second Commit

--- src/main/java/com/company/HelloTest.java
+++ src/main/java/com/company/HelloTest.java
@@ -3,5 +3,6 @@ package com.company;
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello IEDA Git");
+        System.out.println("hello IEDA Git2");
     }
 }
